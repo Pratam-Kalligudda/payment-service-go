cda4f61a733b55f6692832c2373849d20391edbf
PratamCiklum <pkal@ciklum.com>
2025-07-02 14:19:46 +0530

    config added

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+
+	"github.com/Pratam-Kalligudda/payment-service-go/config"
+)
+
+func main() {
+	_, err := config.SetupConfig()
+	if err != nil {
+		log.Fatal("error while loading config : " + err.Error())
+	}
 }
--- config/config.go
+++ config/config.go
@@ -1,12 +1,49 @@
 package config
 
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type AppConfig struct {
-	DNS string
-	SECRET string
-	PORT string
-	ORDERURL string
+	DNS        string
+	SECRET     string
+	PORT       string
+	ORDERURL   string
 	PRODUCTURL string
 }
 
-func init(){
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load env")
+	}
+}
+
+func SetupConfig() (AppConfig, error) {
+	dns := os.Getenv("DNS")
+	if len(dns) <= 0 {
+		return AppConfig{}, errors.New("couldnt load dns")
+	}
+
+	secret := os.Getenv("SECRET")
+	if len(secret) <= 0 {
+		return AppConfig{}, errors.New("couldnt load secret")
+	}
+	port := os.Getenv("PORT")
+	if len(port) <= 0 {
+		return AppConfig{}, errors.New("couldnt load port")
+	}
+	orderUrl := os.Getenv("ORDERURL")
+	if len(orderUrl) <= 0 {
+		return AppConfig{}, errors.New("couldnt load orderUrl")
+	}
+	productUrl := os.Getenv("PRODUCTURL")
+	if len(productUrl) <= 0 {
+		return AppConfig{}, errors.New("couldnt load productUrl")
+	}
+	return AppConfig{DNS: dns, SECRET: secret, PORT: port, ORDERURL: orderUrl, PRODUCTURL: productUrl}, nil
 }
